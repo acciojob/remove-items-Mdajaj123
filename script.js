12049e58160441d0c9a5a970dc0e2a9c28429ba7
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-11-03 19:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // const button=document.querySelector("input")[0];
 function solve(){
-	const select=document.querySelector("select");
+	const select=document.getElementById("colorSelect");
  const selectedvalue=select.value;
 	let option=select.children;
 	for(let i=0;i<option.length;i++){
